51fbe6f0cf1e7d4fbbab91adffd01eeb2d039a71
Syed Abbubakar <syed8040@gmail.com>
2024-11-21 19:13:06 +0530

    some changes

--- api/server.js
+++ api/server.js
@@ -4,17 +4,36 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import postRoute from './routes/postRoute.js';
+import path from "path";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const _dirname = path.resolve()
+
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
 app.use('/uploads', express.static('uploads'));
 app.use('/posts', postRoute);
+
+app.use(express.static(path.join(_dirname, '/client/dist')))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(_dirname, 'client', 'dist', 'index.html'))
+})
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
 
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URI + process.env.MONGO_NAME, { useNewUrlParser: true, useUnifiedTopology: true })
